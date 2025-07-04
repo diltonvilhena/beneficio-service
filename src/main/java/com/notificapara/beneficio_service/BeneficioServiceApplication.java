a0b027550efc7f58ee0a66394abb77acaeffdaca
19/05 1.1 <diltonvilhena@gmail.com>
2025-07-03 21:32:44 -0300

    chore: inicial commit do beneficio-service (skeleton)

---
+++ src/main/java/com/notificapara/beneficio_service/BeneficioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.notificapara.beneficio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notificapara/beneficio_service/BeneficioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificapara.beneficio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
